d23312cd78176b098c57236082fba91cdeb0d51e
Fabien CHOPIN <Fabien.CHOPIN@cea.fr>
2025-06-12 10:21:27 +0200

    Start of a notebook to see if rejection sampler works as intended + change in pyproject.toml requirements

--- samplers/rejection_sampler.py
+++ samplers/rejection_sampler.py
@@ -1,37 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def rejection_sampling_uniform(f, xmin, xmax, N):
+def rejection_sampling_uniform(f, xmin, xmax, N,  *args, **kwargs):
     """
     Rejection sampling with a uniform proposal on [xmin, xmax].
     This algorithm is valid iff f(x) < M.
     If we encounter a value of f(x) exceeding the current estimate of M, we reset.
 
-    Parameters:
-    - f     : target density function (proportional), defined on [xmin, xmax]
-    - xmin  : lower bound of the domain
-    - xmax  : upper bound of the domain
-    - N     : number of samples desired
+    Parameters
+    ----------
+    f : callable
+        Target density function (proportional) defined on [xmin, xmax].
+        Should accept a float or numpy array as first argument, followed by optional arguments.
+    xmin : float
+        Lower bound of the domain.
+    xmax : float
+        Upper bound of the domain.
+    N : int
+        Number of samples to generate.
+    *args : tuple, optional
+        Additional positional arguments to pass to the target function `f`.
+    **kwargs : dict, optional
+        Additional keyword arguments to pass to the target function `f`.
 
-    Returns:
-    - samples : numpy array of length N containing the accepted samples
+    Returns
+    -------
+    samples : np.ndarray
+        Array of length `N` containing the accepted samples.
+    M : float
+        Final estimate of the maximum value of f(x) encountered during sampling
     """
     
     x0 = np.random.uniform(xmin, xmax)
-    M = f(x0)        # First estimate of M
+    M = f(x0, *args, **kwargs)        # First estimate of M
     samples = []
 
     while len(samples) < N:
         X = np.random.uniform(xmin, xmax) # Proposal
-        fX = f(X)
-        if f(X) > M:
+        fX = f(X, *args, **kwargs)
+        if fX > M:
             M = fX
             samples = []
             continue
 
         U = np.random.uniform(0, 1) # Likelihood ratio
-        if U <= fX:
+        if U <= fX / M:
             samples.append(X)
 
-    return np.array(samples)
+    return np.array(samples), M
 
